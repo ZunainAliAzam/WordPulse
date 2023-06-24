a474269a14a2ea9f2b010702bda95034739ea76e
ZunainAliAzam <zunainazam456@gmail.com>
2023-06-24 23:01:32 +0500

    updated

--- searchString.js
+++ searchString.js
@@ -17,10 +17,28 @@ function loadBook(book,displayName) {
         if(request.readyState==4 && request.status==200){
             currentBook = request.responseText;
 
+            //how to remove line breaks and carriage returns and replace them with a br  
+
+            currentBook = currentBook.replace(/(?:\r\n|\r|\n)/g, '<br>');
+
             document.getElementById("fileContent").innerHTML=currentBook;
             
             var newBook = document.getElementById("fileContent");
             newBook.scrollTop = 0;
         }
-    }
+    };
+}
+
+function getBookStats(fileContent){
+
+    var bookLength=document.getElementById("bookLength");
+    var wordCount=document.getElementById("wordCount");
+    var charCount=document.getElementById("charCount");
+
+    let text = fileContent.toLowerCase();
+    let wordArray = text.match(/\b\S+\b/g);
+    let wordDict= {};
+
+    // count every word in the word array
+    
 }
